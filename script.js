43b0937a5f7326c51da34ab18afa11b247147e6d
Massamba-Bienvenu <boukamass@gmail.com>
2022-02-06 21:58:51 +0100

    Getting current location coordinates

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// Getting current location coordinates
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    //1. success callback
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.fr/maps/@${latitude},${longitude}`);
+    },
+    //2. fail callback
+    function () {
+      alert("Could not get your position");
+    }
+  );
+}
