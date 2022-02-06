788f3910af0a37e074c86f11f41ee585720225af
Massamba-Bienvenu <boukamass@gmail.com>
2022-02-06 22:16:22 +0100

    Added a leaflet map

--- script.js
+++ script.js
@@ -23,15 +23,26 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// Getting current location coordinates
+// DISPLAYING A LEAFLET MAP
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     //1. success callback
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.fr/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map("map").setView(coords, 13);
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
+
     //2. fail callback
     function () {
       alert("Could not get your position");
